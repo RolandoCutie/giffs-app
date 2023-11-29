881ddd94c0a02ebc94e4afcc50fc04f3641a8e31
Rolando Garcia Cutie <rolando.garcia@alivi.com>
2023-11-29 12:32:38 -0500

    added gisf search and gifs list

---
+++ src/app/gifs/pages/HomePage/HomePage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gifs-homepage',
+  templateUrl: './HomePage.component.html',
+  styleUrls: []
+})
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
