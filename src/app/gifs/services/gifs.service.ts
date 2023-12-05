d7c22ad4552097ed086fb003283c4b875e150294
Rolando Garcia Cutie <rolando.garcia@alivi.com>
2023-12-05 14:30:33 -0500

    add http client module -make and http petition and obtain a data -create an interface that will process the data

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,32 +1,46 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  public gifList: Gif[] = [];
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'glCEiBo7Hg3dZB1vfH6gD5QixLERwgCw';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
-  private organizedHistory(tag:string){
-    tag=tag.toLowerCase();
-    if(this._tagsHistory.includes(tag)){
-      this._tagsHistory=this._tagsHistory.filter((oldTag)=>oldTag!==tag)
+  private organizedHistory(tag: string) {
+    tag = tag.toLowerCase();
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
     }
 
     this._tagsHistory.unshift(tag);
-    this._tagsHistory=this._tagsHistory.splice(0,10);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
   }
 
   searchTag(tag: string): void {
     if (tag.length === 0) return;
 
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag);
+
     this.organizedHistory(tag);
 
-  
+    this.http
+      .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((response) => {
+        this.gifList = response.data;
+      });
   }
 }
